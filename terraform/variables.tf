6ffbe4d5dbf3cc5a3986cc207dcc53097c1a46ea
RajulNCI <x24197483@student.ncirl.ie>
2026-03-26 21:41:52 +0530

    terraform: add variables

---
+++ terraform/variables.tf
@@ -0,0 +1,34 @@
+variable "aws_region" {
+  description = "AWS region where all resources will be created"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "environment" {
+  description = "dev, staging, or prod"
+  type        = string
+  default     = "dev"
+
+  validation {
+    condition     = contains(["dev", "staging", "prod"], var.environment)
+    error_message = "Environment must be dev, staging, or prod."
+  }
+}
+
+variable "project_name" {
+  description = "Prefix for all resource names"
+  type        = string
+  default     = "verichain"
+}
+
+variable "lambda_memory_mb" {
+  description = "RAM for Lambda in megabytes"
+  type        = number
+  default     = 512
+}
+
+variable "lambda_timeout_seconds" {
+  description = "Max seconds Lambda can run before being killed"
+  type        = number
+  default     = 30
+}
